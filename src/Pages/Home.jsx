caf500bf9c54307571a00f0618ec856ff655914d
shaid30044 <kaona30044@gmail.com>
2023-11-23 20:27:33 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Dream Forge</title>
+      </Helmet>
+    </div>
+  );
+};
+
+export default Home;
