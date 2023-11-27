0d079342cfe0acb32aaccffef18222f9a00d1e46
shaid30044 <kaona30044@gmail.com>
2023-11-27 22:31:01 +0600

    create admin route

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,25 +1,26 @@
 import { useContext } from "react";
-import { AuthContext } from "../Providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
+import useAdmin from "../Hooks/useAdmin";
 import Loading from "../Shared/Loading";
 
-const PrivateRoutes = ({ children }) => {
+const AdminRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
 
-  if (loading) {
+  const [isAdmin, isAdminLoading] = useAdmin();
+
+  if (loading || isAdminLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
         <Loading />
       </div>
     );
   }
-
-  if (user) {
+  if (user && isAdmin) {
     return children;
   }
-
   return <Navigate state={location.pathname} to="/login" />;
 };
 
-export default PrivateRoutes;
+export default AdminRoute;
