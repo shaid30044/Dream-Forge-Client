33c0bdbc18019a0b0e555c5350251500fdb06819
shaid30044 <kaona30044@gmail.com>
2023-11-28 17:37:26 +0600

    get images from database in Instagram section

--- src/Components/Home/Instagram.jsx
+++ src/Components/Home/Instagram.jsx
@@ -3,17 +3,27 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Keyboard } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import { useEffect, useState } from "react";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
 const Instagram = () => {
-  const images = [
-    { image: "https://i.ibb.co/4JGfSyx/image-16-3.png" },
-    { image: "https://i.ibb.co/QfK0f6Z/image-20-2.png" },
-    { image: "https://i.ibb.co/Dtmh4kB/image-17-3.png" },
-    { image: "https://i.ibb.co/M7zyngb/image-19-2.png" },
-    { image: "https://i.ibb.co/5j4fb78/image-18-2.png" },
-    { image: "https://i.ibb.co/FxBPts4/image-21-2.png" },
-    { image: "https://i.ibb.co/9WVS6fR/image-22-2.png" },
-  ];
+  const axiosPublic = useAxiosPublic();
+
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await axiosPublic.get("/instagram");
+
+        setImages(response.data);
+      } catch (error) {
+        console.error("Error fetching Instagram images:", error);
+      }
+    };
+
+    fetchImages();
+  }, [axiosPublic]);
 
   return (
     <div className="font-open px-4 md:px-10 lg:px-20 pb-20 lg:pb-32">
